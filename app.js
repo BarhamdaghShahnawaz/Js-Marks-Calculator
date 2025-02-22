976fd174ae895c35c85d002f78034c5686b73d26
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-02-23 04:39:18 +0500

    completely done

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 var totalMarks = 500;
 
-var username =  +prompt("Enter your name:");
+var username = +prompt("Enter your name:");
 var EnglishMarks = +prompt("Enter your English Marks:");
 var ComputerMarks = +prompt("Enter your Computer Marks:");
-var MathsMarks =  +prompt("Enter your Maths Marks:");
+var MathsMarks = +prompt("Enter your Maths Marks:");
 var PhysicsMarks = +prompt("Enter your Physics Marks:");
 var ChemistryMarks = +prompt("Enter your Chemistry Marks:");
 
