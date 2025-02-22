c80cde49c6a976200a705a5dcc1bd301fbca49c1
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-02-23 04:21:50 +0500

    done

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+var totalMarks = 500;
+
+var username =  +prompt("Enter your name:");
+var EnglishMarks = +prompt("Enter your English Marks:");
+var ComputerMarks = +prompt("Enter your Computer Marks:");
+var MathsMarks =  +prompt("Enter your Maths Marks:");
+var PhysicsMarks = +prompt("Enter your Physics Marks:");
+var ChemistryMarks = +prompt("Enter your Chemistry Marks:");
+
+var studentobtainedmarks = EnglishMarks + ComputerMarks + MathsMarks + PhysicsMarks + ChemistryMarks;
+
+var percentage = (studentobtainedmarks / totalMarks) * 100;
+console.log("Percentage: " + percentage + "%");
